28396aa39cb2070df92ad4812e25a16a5b274b93
han feng <hfcc8685@gamil.com>
2016-01-12 13:37:40 +0800

    [hf] try to use material-ui

--- WebAPP/app/test.js
+++ WebAPP/app/test.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import Paper from 'material-ui/lib/paper';
+import Divider from 'material-ui/lib/divider';
+import TextField from 'material-ui/lib/text-field';
+
+const underlineStyle = {
+  display: 'none',
+};
 
 const style = {
-  height: 100,
-  width: 100,
-  margin: 20,
-  textAlign: 'center',
-  display: 'inline-block',
+  marginLeft: 20,
 };
 
 export default class Test extends React.Component{
 	render() {
 		return (
-			<div>
-				<Paper style={style} zDepth={5}>
-					Hello World!
-				</Paper>
-			</div>
+			<Paper zDepth={2}>
+				<TextField hintText="First name" underlineStyle={underlineStyle} style={style} />
+				<Divider />
+				<TextField hintText="Middle name" underlineStyle={underlineStyle} style={style} />
+				<Divider />
+				<TextField hintText="Last name" underlineStyle={underlineStyle} style={style} />
+				<Divider />
+				<TextField hintText="Email address" underlineStyle={underlineStyle} style={style} />
+				<Divider />
+			</Paper>
 		);
 	}
 }
