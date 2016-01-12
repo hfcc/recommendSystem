8a30423834becb2369a62df49535300a08accd57
han feng <hfcc8685@gamil.com>
2016-01-12 13:05:56 +0800

    [hf] import material-ui to project & test it

--- WebAPP/app/index.js
+++ WebAPP/app/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Test from './test.js';
-
-main();
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 function main(){
 	ReactDOM.render(<Test />, document.getElementById('container'));
 }
+
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
+main();
+
+
 
 
 
--- WebAPP/app/test.js
+++ WebAPP/app/test.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Paper from 'material-ui/lib/paper';
+
+const style = {
+  height: 100,
+  width: 100,
+  margin: 20,
+  textAlign: 'center',
+  display: 'inline-block',
+};
 
 export default class Test extends React.Component{
 	render() {
 		return (
-			<div>hello world ! i love the world.</div>
+			<div>
+				<Paper style={style} zDepth={5}>
+					Hello World!
+				</Paper>
+			</div>
 		);
 	}
 }
