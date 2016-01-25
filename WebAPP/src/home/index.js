b3b0882d22c14945cae65f4c03b809362e22cf0e
han feng <hfcc8685@gamil.com>
2016-01-25 13:17:29 +0800

    [hf] remove test.js

--- WebAPP/src/home/index.js
+++ WebAPP/src/home/index.js
@@ -3,8 +3,6 @@ import { render } from 'react-dom';
 import $ from 'jquery';
 import { Router, Route, Link } from 'react-router';
 
-import Test from '../test';
-
 class Home extends React.Component {
     render() {
         return (
